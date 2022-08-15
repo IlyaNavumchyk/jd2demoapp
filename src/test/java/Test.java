2eeb3133ac1a8f9a5312bd50457752adebce1593
NAUM <naum.st.123@gmail.com>
2022-08-15 21:50:16 +0300

    Add Test
    (cherry picked from commit e618d85acc7fd64b12a66843547e3bbe9eaff3d6)
    
    # Conflicts:
    #	src/test/java/Test.java

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,7 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("git");
-        System.out.println("Test ");
-        System.out.println("lol");
+        System.out.println("Test");
     }
 }
